b55ec363ae15dbc91fa4f72640d9adc598c7a0a7
Andi Kleen <ak@linux.intel.com>
2011-08-06 23:02:15 -0700

    format fixes in header

--- snappy.h
+++ snappy.h
@@ -5,12 +5,12 @@
 #define uint16 uint16_t
 #define uint64 uint64_t
 
-bool snappy_uncompress(const char* compressed, size_t n, char* uncompressed) ;
-int snappy_compress(const char* input,
+bool snappy_uncompress(const char *compressed, size_t n, char *uncompressed) ;
+int snappy_compress(const char *input,
 		    size_t input_length,
-		    char* compressed,
-		    size_t* compressed_length);
-bool snappy_uncompressed_length(const char *buf, size_t len, size_t* result);
+		    char *compressed,
+		    size_t *compressed_length);
+bool snappy_uncompressed_length(const char *buf, size_t len, size_t *result);
 size_t snappy_max_compressed_length(size_t source_len);
 
 
