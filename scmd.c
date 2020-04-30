andikleen/snappy-c

commit d23eef9f542d0bf04e3d766b8ebc24d86467e587

scmd.c: fix memory leak problem.

scmd.c
@@ -115,12 +115,17 @@ int main(int ac, char **av)
 	
 	out = xmalloc(outlen);
 
+	struct snappy_env env;
 	if (mode == compress) {
-		struct snappy_env env;
 		snappy_init_env(&env);
 		err = snappy_compress(&env, map, size, out, &outlen);
 	} else
 		err = snappy_uncompress(map, size, out);
+
+	if (mode == compress) {
+		snappy_free_env(&env);
+	}
+	unmap_file(map, size);
 
 	if (err) {
 		fprintf(stderr, "Cannot process %s: %s\n", av[optind], 
@@ -148,6 +153,9 @@ int main(int ac, char **av)
 			strerror(errno));
 		err = 1;
 	}
-
+	
+	free(file);
+	free(out);
+	
 	return err;
 }

commit 23724d9eb003e3c2d2e4c96453dd8ac609426ab0

Don't free static string
Fix a regression caused by
d23eef9 scmd.c: fix memory leak problem

When outputing data to stdout file is a constant string and cannot be
freed.

Fixes #21

scmd.c
@@ -153,8 +153,9 @@ int main(int ac, char **av)
 			strerror(errno));
 		err = 1;
 	}
-	
-	free(file);
+
+	if (!to_stdout)
+		free(file);
 	free(out);
 	
 	return err;